8c454cbb8062a7c37cf5fbcef51ed91c7b51d4fe
macabeus <bruno.macabeus@gmail.com>
2019-11-01 18:53:07 +0000

    server: add cors

--- app_wine_server/src/index.js
+++ app_wine_server/src/index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const cors = require('@koa/cors')
 const bodyParser = require('koa-bodyparser')
 const router = require('./routes')
 const database = require('./middleware/database')
@@ -6,6 +7,7 @@ const database = require('./middleware/database')
 const app = new Koa()
 
 app
+  .use(cors())
   .use(database)
   .use(bodyParser())
   .use(router.routes())
